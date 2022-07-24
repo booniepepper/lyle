7cb2be64ff37c970481840db0cbd32db67bfcde9
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-07-24 01:11:29 -0700

    Add a very basic CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+/// The current version of the CLI. (As defined in Cargo.toml)
+pub const LYLE_VERSION: &str = std::env!("CARGO_PKG_VERSION");
+
+pub const HELP_MSG: &str = "Usage: lyle FILE";
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<_>>();
+
+    if args.contains(&"--version".into()) {
+        println!("lyle {}", LYLE_VERSION);
+        std::process::exit(0);
+    } else if args.contains(&"--help".into()) {
+        println!("{}", HELP_MSG);
+        std::process::exit(0);
+    }
+
+    // TODO: -o/--output=FILE and others
+
+    // TODO: Allow compiling multiple files (including headers)
+
+    if args.len() > 1 {
+        eprintln!("{}", HELP_MSG);
+        eprintln!(
+            "Error: Expected 1 argument of FILE but got {} arguments.",
+            args.len()
+        );
+        std::process::exit(1);
+    }
+
+    println!("ok bye")
 }
