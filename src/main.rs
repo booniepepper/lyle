ac007cf0dcda540de97aa03db72afe311ee49aa1
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-07-24 01:16:15 -0700

    Abstract out CLI from main()

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ pub const LYLE_VERSION: &str = std::env!("CARGO_PKG_VERSION");
 pub const HELP_MSG: &str = "Usage: lyle FILE";
 
 fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
+    let source_file = parse_args_for_source_file();
+
+    println!("Is this your card? {}", source_file)
+}
+
+fn parse_args_for_source_file() -> String {
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
 
     if args.contains(&"--version".into()) {
         println!("lyle {}", LYLE_VERSION);
@@ -18,7 +24,7 @@ fn main() {
 
     // TODO: Allow compiling multiple files (including headers)
 
-    if args.len() > 1 {
+    if args.len() > 1 || args.is_empty() {
         eprintln!("{}", HELP_MSG);
         eprintln!(
             "Error: Expected 1 argument of FILE but got {} arguments.",
@@ -27,5 +33,5 @@ fn main() {
         std::process::exit(1);
     }
 
-    println!("ok bye")
+    args.get(0).unwrap().to_owned()
 }
